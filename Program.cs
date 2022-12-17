2d564449d7024a03592f5b1fc64b375ee77494f8
Daniel Vinci <me@danielvinci.com>
2022-12-16 20:34:45 -0700

    make program do stuff

--- Program.cs
+++ Program.cs
@@ -5,5 +5,7 @@ internal static class Program
     private static void Main()
     {
         Console.WriteLine("hy world :)");
+        var container = new Container();
+        container.Run((Bar bar) => Console.WriteLine("The number is " + bar.GetNum()));
     }
 }
