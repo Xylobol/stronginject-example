47a20b2c64ce7e25a6e53add1b31318cc652f63a
Daniel Vinci <me@danielvinci.com>
2022-12-16 20:20:42 -0700

    create DataSource

---
+++ DataSource.cs
@@ -0,0 +1,17 @@
+﻿namespace stronginject_example;
+
+public class DataSource
+{
+    private readonly string _epic;
+
+    public DataSource()
+    {
+        Console.WriteLine("Hello from the DataSource constructor");
+        _epic = "this could also be a database connection or something";
+    }
+
+    public string GetEpic()
+    {
+        return _epic;
+    }
+}
