fb14ace08bbef1a93b7848cbc4caaff515698351
Daniel Vinci <me@danielvinci.com>
2022-12-16 20:28:21 -0700

    add bar

---
+++ Bar.cs
@@ -0,0 +1,18 @@
+﻿namespace stronginject_example;
+
+public class Bar
+{
+    private readonly int _someNumber;
+    
+    public Bar(DataSource dataSource)
+    {
+        Console.WriteLine("Hello from the Bar constructor");
+        Console.WriteLine("Epic is " + dataSource.GetEpic());
+        _someNumber = 536;
+    }
+
+    public int GetNum()
+    {
+        return _someNumber;
+    }
+}
