e739d81b840193097aaaf956a243567b5cea19ec
Daniel Vinci <me@danielvinci.com>
2022-12-16 20:35:24 -0700

    bar should also be dependent on datasource

--- Bar.cs
+++ Bar.cs
@@ -4,9 +4,10 @@ public class Bar
 {
     private readonly int _someNumber;
     
-    public Bar(Foo foo)
+    public Bar(DataSource dataSource, Foo foo)
     {
         Console.WriteLine("Hello from the Bar constructor");
+        Console.WriteLine("Epic is " + dataSource.GetEpic());
         Console.WriteLine("The guid is " + foo.GetGuid());
         _someNumber = 536;
     }
