7e858cf0e87912aa00d307e0a8f8a4c8f82ea616
Daniel Vinci <me@danielvinci.com>
2022-12-16 20:17:03 -0700

    use old main file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace stronginject_example;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("hy world :)");
+    }
+}
