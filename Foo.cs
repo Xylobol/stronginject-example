eb78adf2e0a1c5757d4a811062b70304a92a89ba
Daniel Vinci <me@danielvinci.com>
2022-12-16 20:26:19 -0700

    make some changes to foo

--- Foo.cs
+++ Foo.cs
@@ -2,9 +2,17 @@
 
 public class Foo
 {
+    private readonly string _someGuid;
+    
     public Foo(DataSource dataSource)
     {
         Console.WriteLine("Hello from the Foo constructor");
         Console.WriteLine("Epic is " + dataSource.GetEpic());
+        _someGuid = Guid.NewGuid().ToString();
+    }
+
+    public string GetGuid()
+    {
+        return _someGuid;
     }
 }
